4d488a717b42da562888365e9401c2de9ad2c8f4
Chris Piepenbring <cpiepenbring@contractor.5amsolutions.com>
2016-12-30 12:52:49 -0500

    Fixed comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Load the http module to create an http server.
 var http = require('http');
 
-// Configure our HTTP server to respond with Hello World to all requests.
+// Configure our HTTP server to respond with a simple HTML page to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.end("<html><head><title>Stelligent Mini-Project: Chris Piepenbring</title></head>\n<body><h2>Automation for the People</h2></body></html>\n");
